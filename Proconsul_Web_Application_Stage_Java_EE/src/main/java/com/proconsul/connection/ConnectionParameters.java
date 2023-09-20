37d6d220f6f9e247ce3887e989dd9610022067d8
rossimon00 <141406544+rossimon00@users.noreply.github.com>
2023-09-20 12:42:39 +0200

    Add files via upload

---
+++ Proconsul_Web_Application_Stage_Java_EE/src/main/java/com/proconsul/connection/ConnectionParameters.java
@@ -0,0 +1,13 @@
+package com.proconsul.connection;
+
+public interface ConnectionParameters {
+
+	static final String dbDriver = "com.mysql.cj.jdbc.Driver";
+	
+	static final String dbUrl = "jdbc:mysql://localhost:3306/proconsul";
+	
+	static final String dbUser = "root";
+	
+	static final String dbPass = "";
+	
+}

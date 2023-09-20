743d04f935c481037f6fc2172c17aa4e3c4b8418
rossimon00 <141406544+rossimon00@users.noreply.github.com>
2023-09-20 12:49:35 +0200

    Add files via upload

---
+++ Proconsul_Web_Application_Stage_Java_EE/src/main/java/com/proconsul/connection/ConnectionManager.java
@@ -0,0 +1,78 @@
+package com.proconsul.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionManager implements ConnectionParameters {
+
+	
+	public static Connection getConnection() {
+
+		Connection con = null;
+
+		try {
+
+			con = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+		return con;
+	}
+
+	
+	public static PreparedStatement getPreparedStatment(Connection con, String sql) {
+
+		PreparedStatement ps = null;
+
+		try {
+
+			ps = con.prepareStatement(sql);
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+		return ps;
+	}
+
+	
+	public static ResultSet getResultSet(Connection con, String sql) {
+
+		Statement stmt = null;
+
+		ResultSet rs = null;
+
+		try {
+
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+		return rs;
+	}
+
+	
+	public static void closeConnection(Connection con) {
+
+		try {
+
+			con.close();
+
+		} catch (SQLException ex) {
+
+			ex.printStackTrace();
+		}
+	}
+}
